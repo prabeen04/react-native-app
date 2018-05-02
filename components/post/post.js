e30db65c33905122e05957b9f27eabc5d12a50ec
Prabeen04 <prabeen.strange@gmail.com>
2018-05-02 20:13:24 +0530

    posts fetched from api using fetch api

--- components/post/post.js
+++ components/post/post.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+const baseUrl = 'https://prabeen-restapi.herokuapp.com/api/posts';
 
 class Post extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            posts: []
+        }
+    }
+    componentDidMount(){
+        fetch(baseUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            posts = data
+        })
+    }
   render() {
     return (
       <View>
         <Text> Post Component </Text>
+        
+        {this.state.posts.map(post=>{
+            console.debug(post)
+            return <Text>{ post.title} </Text>
+        })}
       </View>
     )
   }
